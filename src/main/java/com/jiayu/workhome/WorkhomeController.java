782729264d2070849fa95c7c3de5f2a718285ed0
康康 <lixiaokang@jiayuinc.com>
2019-01-25 15:54:44 +0800

    代码优化4

--- src/main/java/com/jiayu/workhome/WorkhomeController.java
+++ src/main/java/com/jiayu/workhome/WorkhomeController.java
@@ -63,7 +63,7 @@ public class WorkhomeController {
                         }
                     }
                     count.incrementAndGet();
-                    System.out.println(44444);
+                    System.out.println(5555);
 
                 }
             }
